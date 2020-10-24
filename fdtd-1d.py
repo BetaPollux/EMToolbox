2e53ebc0e4ecf78f7f12c3d2c53e7dbd094cf448
BetaPollux <beta.pollux@gmail.com>
2020-10-24 16:16:21 -0400

    Added dielectric, other cleanup

--- fdtd-1d.py
+++ fdtd-1d.py
@@ -7,27 +7,45 @@ import matplotlib.animation as animation
 
 
 class Grid1D:
-    def __init__(self, n, width):
-        self.x = np.linspace(0, width, n)
-        self.ex = np.zeros(n)
-        self.hy = np.zeros(n)
+    def __init__(self, ndx, width):
+        self.ndx = ndx
+        self.x, self.dx = np.linspace(0, width, ndx, retstep=True)
+        self.dt = self.dx / (2 * 3e8)
+        self.ex = np.zeros(ndx)
+        self.hy = np.zeros(ndx)
+        self.cb = 0.5 * np.ones(ndx)
+        er = 4.0
+        self.cb[self.x > width/2] /= er
         self.sources = []
         self.data = []
 
-    def solve(self, num_steps):
-        self.t = np.arange(num_steps)
+    def solve(self, total_time, n_frames=125):
+        self.t = np.arange(total_time, step=self.dt)
+        frame_ids = np.linspace(0, len(self.t) - 1,
+                                int(n_frames), dtype='int64')
 
-        for time in self.t:
-            for xi in range(1, len(self.ex)):
-                self.ex[xi] = self.ex[xi] + 0.5*(self.hy[xi - 1] - self.hy[xi])
+        abc_left = [0, 0]
+        abc_right = [0, 0]
+
+        for time_id, time in enumerate(self.t):
+            for i in range(1, self.ndx):
+                self.ex[i] = self.ex[i] + self.cb[i] * (
+                              self.hy[i - 1] - self.hy[i])
 
             for source in self.sources:
                 self.ex[source['position']] += source['source'].solve(time)
 
-            for xi in range(len(self.hy) - 1):
-                self.hy[xi] = self.hy[xi] + 0.5*(self.ex[xi] - self.ex[xi + 1])
+            self.ex[0] = abc_left.pop()
+            abc_left.insert(0, self.ex[1])
+            self.ex[-1] = abc_right.pop()
+            abc_right.insert(0, self.ex[-2])
 
-            self.data.append(self.ex.copy())
+            for i in range(self.ndx - 1):
+                self.hy[i] = self.hy[i] + 0.5 * (
+                              self.ex[i] - self.ex[i + 1])
+
+            if time_id in frame_ids:
+                self.data.append(self.ex.copy())
 
     def add_source(self, source, x):
         self.sources.append({'source': source,
@@ -36,12 +54,22 @@ class Grid1D:
 
 
 class Gaussian:
-    def __init__(self, spread, t0):
+    def __init__(self, amplitude, t0, spread):
         self.spread = spread
         self.t0 = t0
 
     def solve(self, time):
-        return np.exp(-0.5 * ((self.t0 - time)/self.spread) ** 2.0)
+        return self.amplitude * np.exp(
+               -0.5 * ((self.t0 - time)/self.spread) ** 2.0)
+
+
+class Sinusoid:
+    def __init__(self, amplitude, freq):
+        self.freq = freq
+        self.amplitude = amplitude
+
+    def solve(self, time):
+        return self.amplitude * np.sin(2 * np.pi * self.freq * time)
 
 
 def animate(i, *fargs):
@@ -52,24 +80,36 @@ def animate(i, *fargs):
 
 
 def main():
-    ndt = 500
+    total_time = 8e-9
+    n_frames = 125
     ndx = 200
-    grid = Grid1D(ndx, 1.0)
-    grid.add_source(Gaussian(12.0, 40.0), ndx//2)
-    grid.solve(ndt)
+    grid = Grid1D(ndx, 2.0)
+    # source = Gaussian(1.0, 40 * grid.dt, 12 * grid.dt)
+    source = Sinusoid(1.0, 700e6)
+    grid.add_source(source, 5)
+    grid.solve(total_time, n_frames)
 
-    fig, axes = plt.subplots(2, 1)
-    axes[0].plot(grid.ex)
+    fig, axes = plt.subplots(2, 1, sharex=True)
+    axes[0].plot(grid.x, grid.ex)
     axes[0].set_ylabel('Ex')
-    axes[1].plot(grid.hy)
+    axes[1].plot(grid.x, grid.hy)
     axes[1].set_ylabel('Hy')
+    axes[1].set_xlabel('Position (m)')
 
     fig2, ax2 = plt.subplots()
     ax2.set_ylim(-2, 2)
     eline, = ax2.plot(grid.ex)
+    ax2.set_ylabel('Ex')
+    ax2.set_xlabel('Position (m)')
     ani = animation.FuncAnimation(
-        fig2, animate, fargs=(eline, grid.data), interval=20,
-        blit=True, frames=ndt)
+        fig2, animate, fargs=(eline, grid.data), interval=40,
+        blit=True, frames=len(grid.data))
+
+    fig3, ax3 = plt.subplots()
+    ax3.plot(grid.t, source.solve(grid.t))
+    ax3.set_title('Source')
+    ax3.set_ylabel('Ex')
+    ax3.set_xlabel('Time (s)')
 
     plt.show()
 
