f03f89df9428b3d514eb5ddd5b995e1a71be8382
BetaPollux <beta.pollux@gmail.com>
2021-02-17 22:32:20 -0500

    Added 2d poisson solver

--- emtoolbox/fields/poisson_fdm.py
+++ emtoolbox/fields/poisson_fdm.py
@@ -7,25 +7,56 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 
-def poisson_1d(width, x, v_left: float=0, v_right: float=0, N: int=100, charge=None):
+def poisson_1d(x, v_left: float=0, v_right: float=0, N: int=100, charge=None):
     '''One-dimension Poisson equation with fixed potential boundaries.
     Normalized charge density ps/eps can be provided via the charge argument'''
     V = np.zeros_like(x)
     V[0] = v_left
     V[-1] = v_right
+    V[1:-1] = 0.5 * (v_left + v_right)
     for _ in range(N):
+        V_old = np.copy(V)
         V[1:-1] = 0.5 * (V[2:] + V[:-2])
+        err = np.sum(np.abs(V - V_old))
         if charge is not None:
             V -= 0.5 * charge
+    print(f'1D Error {err:.3e}')
     return V
 
 
-def trough_analytical(width, height, X, Y,
-                      v_bottom: float=0, v_right: float=0,
-                      v_top: float=0, v_left: float=0):
-    a = width
-    b = height
-    V = 0
+def poisson_2d(X, Y,
+               v_left: float=0, v_right: float=0,
+               v_top: float=0, v_bottom: float=0,
+               N: int=5000, charge=None):
+    '''Two-dimension Poisson equation with fixed potential boundaries.
+    Normalized charge density ps/eps can be provided via the charge argument'''
+    V = np.zeros_like(X)
+    V[:, 0] = v_left
+    V[:, -1] = v_right
+    V[-1, :] = v_top
+    V[0, :] = v_bottom
+    V[0, 0] = 0.5 * (v_bottom + v_left)
+    V[0, -1] = 0.5 * (v_bottom + v_right)
+    V[-1, 0] = 0.5 * (v_top + v_left)
+    V[-1, -1] = 0.5 * (v_top + v_right)
+    V[1:-1, 1:-1] = 0.25 * (v_bottom + v_right + v_top + v_left)
+    for _ in range(N):
+        V_old = np.copy(V)
+        V[1:-1, 1:-1] = 0.25 * (V[2:, 1:-1] + V[:-2, 1:-1] +
+                                V[1:-1, 2:] + V[1:-1, :-2])
+        err = np.sum(np.abs(V - V_old))
+        if charge is not None:
+            V -= 0.25 * charge
+    print(f'2D Error {err:.3e}')
+    return V
+
+
+def trough_analytical(X, Y,
+                      v_left: float=0, v_right: float=0,
+                      v_top: float=0, v_bottom: float=0):
+    a = np.max(X)
+    b = np.max(Y)
+    V = np.zeros_like(X)
     for n in range(1, 101, 2):
         k1 = 4 / (n * np.pi) * np.sin(n * np.pi * Y / b) / np.sinh(n * np.pi * a / b)
         k2 = 4 / (n * np.pi) * np.sin(n * np.pi * X / a) / np.sinh(n * np.pi * b / a)
@@ -48,13 +79,13 @@ def example_poisson_1d():
     Q = (None, qp, qp + qn)
 
     fig, axes = plt.subplots(len(Q), 1, sharex=True)
-    fig.suptitle(f'V(0)={v_left:.1f}, V({w})={v_right:.1f}')
+    fig.suptitle(f'Finite Difference Method (1D)\n')
     for i, Qi in enumerate(Q):
-        V = poisson_1d(w, x, v_left, v_right, charge=Qi)
+        V = poisson_1d(x, v_left, v_right, charge=Qi)
         axes[i].plot(x, V, color='b')
         if Qi is not None:
             q_axis = axes[i].twinx()
-            q_axis.plot(x, Qi, color='r')
+            q_axis.plot(x, Qi, lw=0.5, color='r')
             q_axis.set_ylabel(r'$\rho$ / $\epsilon$', color='red')
         axes[i].set_ylabel('Potential (V)', color='b')
         axes[i].yaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
@@ -68,11 +99,25 @@ def example_trough():
     x = np.linspace(0, w, 101)
     y = np.linspace(0, h, 51)
     X, Y = np.meshgrid(x, y)
-    V = trough_analytical(w, h, X, Y, v_top=10, v_left=5)
+    V = trough_analytical(X, Y, v_top=10, v_left=5)
+    plt.title('Analytical Solution')
     plt.contour(X, Y, V)
     plt.show()
 
 
+def example_poisson_2d():
+    w = 2.0
+    h = 1.0
+    x = np.linspace(0, w, 101)
+    y = np.linspace(0, h, 51)
+    X, Y = np.meshgrid(x, y)
+    V = poisson_2d(X, Y, v_top=10, v_left=5)
+
+    plt.title('Finite Difference Method (2D)')
+    plt.contour(X, Y, V)
+    plt.show()
+
 if __name__ == '__main__':
     example_poisson_1d()
+    example_poisson_2d()
     example_trough()    
--- tests/test_poisson_fdm.py
+++ tests/test_poisson_fdm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import numpy as np
-from emtoolbox.fields.poisson_fdm import trough_analytical
+import emtoolbox.fields.poisson_fdm as fdm
 import pytest
 from pytest import approx
 
@@ -12,10 +12,22 @@ def test_trough_analytical():
     x = np.linspace(0, w, 4)
     y = np.linspace(0, h, 5)
     X, Y = np.meshgrid(x, y)
-    V = trough_analytical(w, h, X, Y, v_left=10, v_bottom=0, v_top=20, v_right=30)
+    V = fdm.trough_analytical(X, Y, v_left=10, v_bottom=0, v_top=20, v_right=30)
     assert V[3, 1] == approx(16.4784, abs=1e-4)
     assert V[3, 2] == approx(21.8499, abs=1e-4)
     assert V[2, 1] == approx(14.1575, abs=1e-4)
     assert V[2, 2] == approx(20.4924, abs=1e-4)
     assert V[1, 1] == approx(9.60942, abs=1e-4)
     assert V[1, 2] == approx(14.9810, abs=1e-4)
+
+
+def test_poisson_2d():
+    w = 2.0
+    h = 1.0
+    x = np.linspace(0, w, 101)
+    y = np.linspace(0, h, 51)
+    X, Y = np.meshgrid(x, y)
+    V = fdm.poisson_2d(X, Y, v_top=10, v_left=5, N=5000)
+    Va = fdm.trough_analytical(X, Y, v_top=10, v_left=5)
+    error = (Va - V)[1:-1, 1:-1]  # Exclude boundaries
+    assert np.max(error) < 0.01 * np.max(V)
