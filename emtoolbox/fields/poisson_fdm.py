65872490702f95c1f16fd97a07d373825ddbf8eb
BetaPollux <beta.pollux@gmail.com>
2021-02-17 21:35:00 -0500

    Added 1d poisson solver

--- emtoolbox/fields/poisson_fdm.py
+++ emtoolbox/fields/poisson_fdm.py
@@ -4,6 +4,20 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+def poisson_1d(width, x, v_left: float=0, v_right: float=0, N: int=100, charge=None):
+    '''One-dimension Poisson equation with fixed potential boundaries.
+    Normalized charge density ps/eps can be provided via the charge argument'''
+    V = np.zeros_like(x)
+    V[0] = v_left
+    V[-1] = v_right
+    for _ in range(N):
+        V[1:-1] = 0.5 * (V[2:] + V[:-2])
+        if charge is not None:
+            V -= 0.5 * charge
+    return V
 
 
 def trough_analytical(width, height, X, Y,
@@ -21,7 +35,34 @@ def trough_analytical(width, height, X, Y,
     return V
 
 
-if __name__ == '__main__':
+def example_poisson_1d():
+    def dist(x, a0, m, std):
+        return a0 * np.exp(-0.5 * ((x - m) / std) ** 2)
+
+    w = 1.0
+    x = np.linspace(0, w, 100)
+    v_left = -2
+    v_right = 1
+    qp = dist(x, 0.02, 0.25 * w, 0.05 * w)
+    qn = dist(x, -0.03, 0.75 * w, 0.1 * w)
+    Q = (None, qp, qp + qn)
+
+    fig, axes = plt.subplots(len(Q), 1, sharex=True)
+    fig.suptitle(f'V(0)={v_left:.1f}, V({w})={v_right:.1f}')
+    for i, Qi in enumerate(Q):
+        V = poisson_1d(w, x, v_left, v_right, charge=Qi)
+        axes[i].plot(x, V, color='b')
+        if Qi is not None:
+            q_axis = axes[i].twinx()
+            q_axis.plot(x, Qi, color='r')
+            q_axis.set_ylabel(r'$\rho$ / $\epsilon$', color='red')
+        axes[i].set_ylabel('Potential (V)', color='b')
+        axes[i].yaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
+    axes[-1].set_xlim([0, w])
+    plt.show()
+
+
+def example_trough():
     w = 2.0
     h = 1.0
     x = np.linspace(0, w, 101)
@@ -30,3 +71,8 @@ if __name__ == '__main__':
     V = trough_analytical(w, h, X, Y, v_top=10, v_left=5)
     plt.contour(X, Y, V)
     plt.show()
+
+
+if __name__ == '__main__':
+    example_poisson_1d()
+    example_trough()    
