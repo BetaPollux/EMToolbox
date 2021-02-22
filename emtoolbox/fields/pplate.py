7728e779d6d1c8d0af7be037f485ccfa6a50705d
BetaPollux <beta.pollux@gmail.com>
2021-02-21 19:06:18 -0500

    Added ParallelPlates class

---
+++ emtoolbox/fields/pplate.py
@@ -0,0 +1,77 @@
+#!/bin/usr/python3
+
+import numpy as np
+from emtoolbox.utils.constants import EPS0
+
+
+class ParallelPlates:
+    '''Parallel Plate one-dimensional structure with an arbitrary number of dielectric layers'''
+    def __init__(self, er: float, t: float, area: float = 1):
+        '''Define geometry as:
+            Dielelctric constants   er : (er1, er2, ...) unitless
+            Layer thicknesses       t :  (t1, t2, ...) in meters
+            Area                    area: square meters
+        '''
+        if type(er) != type(t):
+            raise Exception('Parameters er and t must be the same type')
+
+        if hasattr(er, '__iter__') and hasattr(t, '__iter__'):
+            if len(er) != len(t):
+                raise Exception('Parameters er and t must be the same length')
+            x0 = []
+            next_x0 = 0
+            for ti in t:
+                x0.append(next_x0)
+                next_x0 += ti
+            self.slabs = list(zip(er, t, x0))
+        else:
+            self.slabs = list(zip([er], [t], [0]))
+
+        self.thickness = np.sum(t)
+        self.area = area
+
+    def capacitance(self):
+        '''Get total capacitance in F'''
+        C = np.array([EPS0 * er / t for er, t, _ in self.slabs])
+        return self.area / np.sum(1 / C)
+
+    def get_arrays(self, N: int = 11):
+        '''Get arrays of position and relative permittivity (X, er)
+        X has length N
+        er is defined at half-grid points, so has length N-1'''
+        X = np.linspace(0, self.thickness, N)
+        cond = [(X < x0 + t) for _, t, x0 in self.slabs]
+        choice = [er for er, *_ in self.slabs]
+        return (X, np.select(cond, choice)[:-1])
+
+    def efield(self, X, Va):
+        '''Get electric field for the specified differential voltage and locations'''
+        if X.min() < 0 or X.max() > self.thickness:
+            raise Exception('Invalid range of X provided')
+        D = self.charge(Va) / self.area
+        cond = [(X <= x0 + t) for _, t, x0 in self.slabs]
+        choice = [D / (EPS0 * er) for er, *_ in self.slabs]
+        return np.select(cond, choice)
+
+    def potential(self, X, Va, Vref=0):
+        '''Get potential for the specified differential voltage and locations.
+        The reference voltage can be used to offset the results'''
+        if X.min() < 0 or X.max() > self.thickness:
+            raise Exception('Invalid range of X provided')
+        D = self.charge(Va) / self.area
+        cond = []
+        choice = []
+        V0 = Vref
+        for er, t, x0 in self.slabs:
+            E = D / (EPS0 * er)
+            cond.append(X <= x0 + t)
+            choice.append(E * (X - x0) + V0)
+            V0 += E * t
+        return np.select(cond, choice)
+
+    def charge(self, Va):
+        return self.capacitance() * Va
+
+    def energy(self, Va):
+        return 0.5 * self.capacitance() * Va ** 2
+ 
---
+++ tests/test_pplate.py
@@ -0,0 +1,142 @@
+#!/bin/usr/python3
+
+import numpy as np
+import pytest
+from pytest import approx
+from emtoolbox.fields.pplate import ParallelPlates
+
+
+def test_capacitance():
+    pp = ParallelPlates(3.5, 1e-3)
+    assert pp.capacitance() == approx(30.989e-9)
+
+
+def test_capacitance_area():
+    pp = ParallelPlates(3.5, 1e-3, area=10.0)
+    assert pp.capacitance() == approx(309.89e-9)
+
+
+def test_get_arrays():
+    N = 11
+    pp = ParallelPlates(3.5, 1e-3)
+    X, er = pp.get_arrays(N=N)
+    assert X == approx(np.linspace(0, 1e-3, N))
+    assert er == approx(3.5 * np.ones(N-1))
+
+
+def test_efield():
+    t = 1e-3
+    N = 5
+    pp = ParallelPlates(3.5, t)
+    X = np.linspace(0, t, N)
+    Va = 10.0
+    expected = Va / t * np.ones(N)
+    efield = pp.efield(X, Va)
+    assert efield == approx(expected)
+
+
+def test_potential():
+    t = 1e-3
+    N = 5
+    pp = ParallelPlates(3.5, t)
+    X = np.linspace(0, t, N)
+    Va = 10.0
+    expected = Va / t * X
+    potential = pp.potential(X, Va)
+    assert potential == approx(expected)
+
+
+def test_potential_ref():
+    t = 1e-3
+    N = 5
+    pp = ParallelPlates(3.5, t)
+    X = np.linspace(0, t, N)
+    Va = 10.0
+    Vref = 5.0
+    expected = Va / t * X + Vref
+    potential = pp.potential(X, Va, Vref=Vref)
+    assert potential == approx(expected)
+
+
+def test_2layer():
+    er1, t1 = 5.0, 1e-3
+    er2, t2 = 1.0, 3e-3
+    pp = ParallelPlates((er1, er2), (t1, t2))
+    X, er = pp.get_arrays(N=5)
+    assert pp.thickness == approx(t1 + t2)
+    assert X == approx([0.0, 1e-3, 2e-3, 3e-3, 4e-3])
+    assert er == approx([er1, er2, er2, er2])
+
+
+def test_2layer_mixed_type():
+    er1, t1 = 5.0, 1e-3
+    er2 = 1.0
+    with pytest.raises(Exception):
+        ParallelPlates((er1, er2), t1)
+
+
+def test_2layer_mixed_length():
+    er1, t1 = 5.0, 1e-3
+    er2, t2 = 1.0, 3e-3
+    with pytest.raises(Exception):
+        ParallelPlates((er1, er2), (t1, t2, t2))
+
+
+def test_capacitance_2layer():
+    er1, t1 = 5.0, 1e-3
+    er2, t2 = 1.0, 3e-3
+    pp = ParallelPlates((er1, er2), (t1, t2))
+    assert pp.capacitance() == approx(2.767e-9)
+
+
+def test_potential_2layer():
+    er1, t1 = 5.0, 1e-3
+    er2, t2 = 1.0, 3e-3
+    pp = ParallelPlates((er1, er2), (t1, t2))
+    X = np.linspace(0, 4e-3, 5)
+    V = pp.potential(X, 200)
+    assert V == approx([0, 12.5, 75, 137.5, 200])
+
+
+def test_efield_2layer():
+    er1, t1 = 5.0, 1e-3
+    er2, t2 = 1.0, 3e-3
+    pp = ParallelPlates((er1, er2), (t1, t2))
+    X = np.linspace(0, 4e-3, 5)
+    E = pp.efield(X, 200)
+    E1 = 12.5 / t1
+    E2 = 187.5 / t2
+    assert E == approx([E1, E1, E2, E2, E2])
+
+
+def test_capacitance_3layer():
+    er1, t1 = 5.0, 1e-3
+    er2, t2 = 1.0, 3e-3
+    pp = ParallelPlates((er2, er1, er2), (0.5 * t2, t1, 0.5 * t2))
+    assert pp.capacitance() == approx(2.767e-9)
+
+
+def test_charge():
+    pp = ParallelPlates(3.5, 1e-3)
+    Va = 10.0
+    expected = pp.capacitance() * Va
+    assert pp.charge(Va) == approx(expected)
+
+
+def test_energy():
+    pp = ParallelPlates(3.5, 1e-3)
+    Va = 10.0
+    expected = 0.5 * pp.capacitance() * Va ** 2
+    assert pp.energy(Va) == approx(expected)
+
+def test_2layer_area():
+    er1, t1 = 5.0, 1e-3
+    er2, t2 = 1.0, 3e-3
+    A = 3.0
+    pp1 = ParallelPlates((er1, er2), (t1, t2))
+    pp2 = ParallelPlates((er1, er2), (t1, t2), area=A)
+    Va = 25.0
+    X = np.linspace(0, t1 + t2, 11)
+    assert pp1.capacitance() == approx(pp2.capacitance() / A)
+    assert pp1.efield(X, Va) == approx(pp2.efield(X, Va))
+    assert pp1.potential(X, Va) == approx(pp2.potential(X, Va))
