e2ff6fad2472c12c3b027ce29dcfeee1e3a6c86d
BetaPollux <beta.pollux@gmail.com>
2021-03-22 21:45:30 -0400

    Refactored WireMtl to use Wire objects

--- emtoolbox/tline/wire_mtl.py
+++ emtoolbox/tline/wire_mtl.py
@@ -1,64 +1,47 @@
 #! /usr/bin/python3
 
-'''Multi-conductor transmission line with round wire conductors'''
+'''Multi-conductor transmission line with round wires'''
 
 import numpy as np
 from emtoolbox.utils.constants import MU0, EPS0
+from emtoolbox.tline.wire import Wire, Plane, Shield
 import emtoolbox.fields.poisson_fdm as fdm
 
 
 class WireMtl():
-    def __init__(self, conductors: list, er: float = 1.0, ref: str = 'wire', rs: float = None):
-        '''Create a wire-type MTL with the given conductors.
-        The conductors are provided as position and radius (x0, y0, rw).
-        The conductors are immersed in a relative permittivity of er
-        ref is the reference conductor, options are:
-            wire
-            plane
-            shield
-        The shield reference requires radius of shield rs'''
-        if ref not in ('wire', 'plane', 'shield'):
-            raise Exception('ref must be wire, plane or shield')
-        if ref == 'wire':
-            self.N = len(conductors) - 1
-        elif ref == 'plane' or ref == 'shield':
-            self.N = len(conductors)
-        if self.N < 1:
-            raise Exception('Requires at least 2 conductors, or a reference plane or shield')
-        if ref == 'shield' and rs is None:
-            raise Exception('Shield requires radius rs')
-        for w in conductors:
-            if type(w) is not tuple or not list:
-                raise Exception(f'Bad conductor type: {w}')
-            if len(w) != 3:
-                raise Exception(f'Bad conductor definition: {w}')
+    def __init__(self, wires: list, ref, er: float = 1.0):
+        '''Create a wire-type MTL with the given wires.
+        The wires are provided as Wire objects
+        The wires are immersed in a relative permittivity of er
+        ref is the reference conductor, of type Wire, Plane or Shield'''
+        if type(ref) not in (Wire, Plane, Shield):
+            raise TypeError('ref must be wire, plane or shield')
+        if len(wires) < 1:
+            raise ValueError('Requires at least 2 wires, or a reference plane or shield')
+        for w in wires:
+            if type(w) is not Wire:
+                raise TypeError(f'Bad wire type')
         # TODO validate positions and radii
-        self.conductors = conductors
+        self.wires = wires
         self.er = er
         self.ref = ref
-        self.rs = rs
     
     def capacitance(self, /, method: str = None, fdm_params: dict = {}) -> np.ndarray:
         '''Calculate the capacitance matrix'''
-        x0, y0, r0 = self.conductors[0]
         if method is None or method.lower() == 'ana':
-            if self.ref == 'wire' and self.N == 1:
-                x1, y1, r1 = self.conductors[1]
-                s = np.sqrt((x1 - x0)**2 + (y1 - y0)**2)
-                return np.array([wire_capacitance(s, r1, r0, self.er)])
-            else:
-                return MU0 * EPS0 * self.er * np.linalg.inv(self.inductance())
+            return MU0 * EPS0 * self.er * np.linalg.inv(self.inductance())
         elif method.lower() == 'fdm':
-            if self.ref != 'wire' or self.N != 1:
-                raise Exception('FDM currently supports only two conductors, no reference')
+            if type(self.ref) is not Wire or len(self.wires) != 1:
+                raise Exception('FDM currently supports only two wires, no reference')
             # Define simulation region
             pad = 20  # Zero potential boundaries must be far away
-            w_list = np.array([[xi - pad * ri for xi, _, ri in self.conductors],
-                               [xi + pad * ri for xi, _, ri in self.conductors]])
-            h_list = np.array([[yi - pad * ri for _, yi, ri in self.conductors],
-                               [yi + pad * ri for _, yi, ri in self.conductors]])
+            w_list = np.array([[wi.x - pad * wi.radius for wi in self.wires],
+                               [wi.x + pad * wi.radius for wi in self.wires]])
+            h_list = np.array([[wi.y - pad * wi.radius for wi in self.wires],
+                               [wi.y + pad * wi.radius for wi in self.wires]])
             # Grid based on wire size or user configured
-            x1, y1, r1 = self.conductors[1]
+            x0, y0, r0 = self.ref.x, self.ref.y, self.ref.radius
+            x1, y1, r1 = self.wires[0].x, self.wires[0].y, self.wires[0].radius
             dx = fdm_params.get('dx', min(r0, r1) / 6)
             x = np.arange(w_list.min(), w_list.max(), dx)
             y = np.arange(h_list.min(), h_list.max(), dx)
@@ -77,58 +60,30 @@ class WireMtl():
             V = fdm.poisson_2d(X, Y, bc=(bc_bool, bc_val), conv=1e-5)
             er = self.er * np.ones_like(X)[:-1, :-1]
             # Calculate charge and capacitances
-            C = np.zeros(len(self.conductors))
-            for i, (xi, yi, ri) in enumerate(self.conductors):
+            C = np.zeros((len(self.wires), len(self.wires)))
+            for i, wi in enumerate(self.wires):
                 # Keep some distance from wire surface, but cannot overlap other wire
                 # TODO make this more robust
-                gpad = ri + 4 * dx
-                xi1 = np.searchsorted(x, xi - gpad)
-                xi2 = np.searchsorted(x, xi + gpad)
-                yi1 = np.searchsorted(y, yi - gpad)
-                yi2 = np.searchsorted(y, yi + gpad)
+                gpad = wi.radius + 4 * dx
+                xi1 = np.searchsorted(x, wi.x - gpad)
+                xi2 = np.searchsorted(x, wi.x + gpad)
+                yi1 = np.searchsorted(y, wi.y - gpad)
+                yi2 = np.searchsorted(y, wi.y + gpad)
                 print(f'({x[xi1]:.3e}, {x[xi2]:.3e}), ({y[yi1]:.3e}, {y[yi2]:.3e}) gauss')
                 C[i] = abs(fdm.gauss_2d(X, Y, V, er, xi1, xi2, yi1, yi2) / V1)
-            if abs((C[1] - C[0]) / C[0]) > 0.05:
-                raise Exception(f'Inconsistent capacitance calculation: {C}')
             return np.array([C[0]])
         else:
             raise Exception(f'Invalid method specified: {method}')
 
     def inductance(self) -> np.ndarray:
         '''Calculate the inductance matrix'''
-        x0, y0, r0 = self.conductors[0]
-        d0 = [np.sqrt((xi - x0)**2 + (yi - y0)**2) for xi, yi, _ in self.conductors[1:]]
-        if self.ref == 'wire' and self.N == 1:
-            *_, r1 = self.conductors[1]
-            L = np.array([wire_inductance(d0[0], r1, r0)])
-        else:
-            L = np.zeros((self.N, self.N))
-            if self.ref == 'wire':
-                for i, (xi, yi, ri) in enumerate(self.conductors[1:]):
-                    L[i, i] = wire_self_inductance(d0[i], ri, r0)
-                    for j, (xj, yj, _) in enumerate(self.conductors[1:]):
-                        if j != i:
-                            dij = np.sqrt((xi - xj)**2 + (yi - yj)**2)
-                            L[i, j] = wire_mutual_inductance(d0[i], d0[j], dij, r0)
-                            L[j, i] = L[i, j]
-            elif self.ref == 'plane':
-                for i, (xi, yi, ri) in enumerate(self.conductors):
-                    L[i, i] = wire_self_inductance_plane(yi, ri)
-                    for j, (xj, yj, _) in enumerate(self.conductors):
-                        if j != i:
-                            dij = np.sqrt((xi - xj)**2 + (yi - yj)**2)
-                            L[i, j] = wire_mutual_inductance_plane(yi, yj, dij)
-                            L[j, i] = L[i, j]
-            elif self.ref == 'shield':
-                for i, (xi, yi, ri) in enumerate(self.conductors):
-                    di = np.sqrt(xi**2 + yi**2)
-                    L[i, i] = wire_self_inductance_shield(di, self.rs, ri)
-                    for j, (xj, yj, _) in enumerate(self.conductors):
-                        if j != i:
-                            dj = np.sqrt(xj**2 + yj**2)
-                            tij = np.arctan2(yi, xi) - np.arctan2(yj, xj)
-                            L[i, j] = wire_mutual_inductance_shield(di, dj, self.rs, tij)
-                            L[j, i] = L[i, j]
+        L = np.zeros((len(self.wires), len(self.wires)))
+        for i, wi in enumerate(self.wires):
+            L[i, i] = wire_self_inductance(wi, self.ref)
+            for j, wj in enumerate(self.wires):
+                if j != i:
+                    L[i, j] = wire_mutual_inductance(wi, wj, self.ref)
+                    L[j, i] = L[i, j]
         return L
 
 
@@ -162,37 +117,50 @@ def wire_inductance(s: float, rw1: float, rw2: float = None) -> float:
         return MU0 / np.pi * np.arccosh(s / (2 * rw1))
 
 
-def wire_self_inductance(di0: float, rw0: float, rwi: float):
-    '''Inductance between wire i and reference wire 0
-    Uses widely separated assumption, di0/rw > 4'''
-    return MU0 / (2 * np.pi) * np.log(di0 ** 2 / (rw0 * rwi))
+def wire_self_inductance(wire, ref):
+    '''Inductance between wire and reference conductor
+    Uses widely separated assumption'''
+    if type(ref) == Wire:
+        di0 = wire.distance_to(ref)
+        rw0 = ref.radius
+        rwi = wire.radius
+        return MU0 / (2 * np.pi) * np.log(di0 ** 2 / (rw0 * rwi))
+    elif type(ref) == Plane:
+        hi = ref.height_of(wire)
+        rwi = wire.radius
+        return MU0 / (2 * np.pi) * np.log(2 * hi / rwi)
+    elif type(ref) == Shield:
+        rs = ref.radius
+        di = wire.offset()
+        rwi = wire.radius
+        return MU0 / (2 * np.pi) * np.log((rs**2 - di**2) / (rs * rwi))
+    else:
+        raise Exception('Unrecognized reference type')
 
 
-def wire_mutual_inductance(di0: float, dj0: float, dij: float, rw0: float):
+def wire_mutual_inductance(wire_i, wire_j, ref):
+    # di0: float, dj0: float, dij: float, rw0: float):
     '''Inductance between wire i and wire j, with reference wire 0
     Uses widely separated assumption, di0/rw > 4'''
-    return MU0 / (2 * np.pi) * np.log(di0 * dj0 / (dij * rw0))
-
-
-def wire_self_inductance_plane(hi: float, rwi: float):
-    '''Inductance between wire i with height hi above reference plane'''
-    return MU0 / (2 * np.pi) * np.log(2 * hi / rwi)
-
-
-def wire_mutual_inductance_plane(hi: float, hj: float, sij: float):
-    '''Inductance between wire i and wire j, above a reference plane'''
-    return MU0 / (4 * np.pi) * np.log(1 + 4 * hi * hj / sij**2)
-
-
-def wire_self_inductance_shield(di: float, rs: float, rwi: float):
-    '''Inductance between wire i with distance di to center of shield with radius rs'''
-    return MU0 / (2 * np.pi) * np.log((rs**2 - di**2) / (rs * rwi))
-
-
-def wire_mutual_inductance_shield(di: float, dj: float, rs: float, tij: float):
-    '''Inductance between wire i and wire j within a shield or radius rs.
-    di and dj are distances to the center, with tij the angle between wires'''
-    return MU0 / (2 * np.pi) * np.log(dj / rs * np.sqrt(
-        ((di*dj)**2 + rs**4 - 2* di * dj * rs**2 * np.cos(tij)) /
-        ((di*dj)**2 + dj**4 - 2* di * dj**3 * np.cos(tij))
-    ))
+    if type(ref) == Wire:
+        rw0 = ref.radius
+        di0 = wire_i.distance_to(ref)
+        dj0 = wire_j.distance_to(ref)
+        dij = wire_i.distance_to(wire_j)
+        return MU0 / (2 * np.pi) * np.log(di0 * dj0 / (dij * rw0))
+    elif type(ref) == Plane:
+        hi = ref.height_of(wire_i)
+        hj = ref.height_of(wire_j)
+        sij = wire_i.distance_to(wire_j)
+        return MU0 / (4 * np.pi) * np.log(1 + 4 * hi * hj / sij**2)
+    elif type(ref) == Shield:
+        rs = ref.radius
+        di = wire_i.offset()
+        dj = wire_j.offset()
+        tij = wire_i.angle_to(wire_j)
+        return MU0 / (2 * np.pi) * np.log(dj / rs * np.sqrt(
+            ((di*dj)**2 + rs**4 - 2* di * dj * rs**2 * np.cos(tij)) /
+            ((di*dj)**2 + dj**4 - 2* di * dj**3 * np.cos(tij))
+        ))
+    else:
+        raise Exception('Unrecognized reference type')
--- tests/test_wire_mtl.py
+++ tests/test_wire_mtl.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 from pytest import approx
 import emtoolbox.tline.wire_mtl as mtl
+from emtoolbox.tline.wire import Wire, Plane, Shield
 import emtoolbox.tline.coax as coax
 
 
@@ -21,7 +22,7 @@ def test_wire_capacitance_er():
     er = 4
     C = mtl.wire_capacitance(s, rw)
     Cer = mtl.wire_capacitance(s, rw, er=er)
-    assert C == approx(Cer / er, abs=0.1e-12)
+    assert C == approx(Cer / er, abs=1e-14)
 
 
 def test_wire_inductance():
@@ -68,88 +69,96 @@ def test_wire_self_inductance():
     # Paul MTL P5.4
     rw = 7.5
     di0 = 50
-    L = mtl.wire_self_inductance(di0, rw, rw)
-    assert L == approx(0.759e-6, rel=0.001)
+    ref = Wire(0, 0, rw)
+    wire = Wire(di0, 0, rw)
+    L = mtl.wire_self_inductance(wire, ref)
+    assert L == approx(0.759e-6, abs=0.001e-6)
 
 
 def test_wire_mutual_inductance():
     # Paul MTL P5.4
     rw = 7.5
-    di0 = 50
-    dj0 = 50
-    dij = 100
-    L = mtl.wire_mutual_inductance(di0, dj0, dij, rw)
+    ref = Wire(0, 0, rw)
+    wi = Wire(-50, 0, rw)
+    wj = Wire(50, 0, rw)
+    L = mtl.wire_mutual_inductance(wi, wj, ref)
     assert L == approx(0.241e-6, rel=0.001)
 
 
-def test_one_conductor():
-    with pytest.raises(Exception):
-        mtl.WireMtl([(0, 0, 1)])
+def test_bad_ref():
+    with pytest.raises(TypeError):
+        mtl.WireMtl([Wire(0, 0, 0.5)], 'Ref!')
 
 
-def test_bad_conductor_types():
-    with pytest.raises(Exception):
-        mtl.WireMtl([1, 1])
+def test_bad_wire():
+    with pytest.raises(TypeError):
+        mtl.WireMtl([Plane()], Plane())
 
 
-def test_bad_conductor_def():
-    with pytest.raises(Exception):
-        mtl.WireMtl([(0, 0, 1), (0, 1)])
+def test_empty_list():
+    with pytest.raises(ValueError):
+        mtl.WireMtl([], Plane())
 
 
 def test_two_wire_capacitance():
     rw = 7.5
     s = 50
-    wires = [(-0.5 * s, 0, rw), (0.5 * s, 0, rw)]
-    pair = mtl.WireMtl(wires)
+    wires = [Wire(-0.5 * s, 0, rw)]
+    ref = Wire(0.5 * s, 0, rw)
+    pair = mtl.WireMtl(wires, ref)
     C = mtl.wire_capacitance(s, rw)
-    assert pair.capacitance()[0] == approx(C)
+    assert pair.capacitance()[[0]] == approx(C, rel=0.02, abs=1e-14)
 
 
 def test_two_wire_capacitance_diagonal():
     rw = 7.5
     s = 50
     x2 = y2 = s / np.sqrt(2)
-    wires = [(0, 0, rw), (x2, y2, rw)]
-    pair = mtl.WireMtl(wires)
+    wires = [Wire(0, 0, rw)]
+    ref = Wire(x2, y2, rw)
+    pair = mtl.WireMtl(wires, ref)
     C = mtl.wire_capacitance(s, rw)
-    assert pair.capacitance()[0] == approx(C)
+    assert pair.capacitance()[[0]] == approx(C, rel=0.02, abs=1e-14)
 
 
 def test_two_wire_capacitance_er():
     er = 4
     rw = 7.5
     s = 50
-    wires = [(-0.5 * s, 0, rw), (0.5 * s, 0, rw)]
-    pair = mtl.WireMtl(wires, er)
+    wires = [Wire(-0.5 * s, 0, rw)]
+    ref = Wire(0.5 * s, 0, rw)
+    pair = mtl.WireMtl(wires, ref, er)
     C = mtl.wire_capacitance(s, rw, er=er)
-    assert pair.capacitance()[0] == approx(C)
+    assert pair.capacitance()[[0]] == approx(C, rel=0.02, abs=1e-14)
 
 
 def test_two_wire_inductance():
     rw = 7.5
     s = 50
-    wires = [(-0.5 * s, 0, rw), (0.5 * s, 0, rw)]
-    pair = mtl.WireMtl(wires)
+    wires = [Wire(-0.5 * s, 0, rw)]
+    ref = Wire(0.5 * s, 0, rw)
+    pair = mtl.WireMtl(wires, ref)
     L = mtl.wire_inductance(s, rw)
-    assert pair.inductance()[0] == approx(L)
+    assert pair.inductance()[[0]] == approx(L, rel=0.02)
 
 
 def test_two_wire_inductance_diagonal():
     rw = 7.5
     s = 50
     x2 = y2 = s / np.sqrt(2)
-    wires = [(0, 0, rw), (x2, y2, rw)]
-    pair = mtl.WireMtl(wires)
+    wires = [Wire(0, 0, rw)]
+    ref = Wire(x2, y2, rw)
+    pair = mtl.WireMtl(wires, ref)
     L = mtl.wire_inductance(s, rw)
-    assert pair.inductance()[0] == approx(L)
+    assert pair.inductance()[[0]] == approx(L, rel=0.02)
 
 
 def test_two_wire_capacitance_bad_method():
     rw = 1e-3
     s = 10e-3
-    wires = [(-0.5 * s, 0, rw), (0.5 * s, 0, rw)]
-    pair = mtl.WireMtl(wires)
+    wires = [Wire(-0.5 * s, 0, rw)]
+    ref = Wire(0.5 * s, 0, rw)
+    pair = mtl.WireMtl(wires, ref)
     with pytest.raises(Exception):
         pair.capacitance(method='junk')
 
@@ -157,8 +166,9 @@ def test_two_wire_capacitance_bad_method():
 def test_two_wire_capacitance_fdm():
     rw = 1e-3
     s = 8e-3
-    wires = [(-0.5 * s, 0, rw), (0.5 * s, 0, rw)]
-    pair = mtl.WireMtl(wires)
+    wires = [Wire(-0.5 * s, 0, rw)]
+    ref = Wire(0.5 * s, 0, rw)
+    pair = mtl.WireMtl(wires, ref)
     expected = pair.capacitance(method='ana')
     C = pair.capacitance(method='fdm')
     assert C == approx(expected, rel=0.05, abs=0.1e-12)
@@ -167,8 +177,9 @@ def test_two_wire_capacitance_fdm():
 def test_two_wire_capacitance_fdm_diagonal():
     rw = 1e-3
     x2 = y2 = 6e-3
-    wires = [(0, 0, rw), (x2, y2, rw)]
-    pair = mtl.WireMtl(wires)
+    wires = [Wire(0, 0, rw)]
+    ref = Wire(x2, y2, rw)
+    pair = mtl.WireMtl(wires, ref)
     expected = pair.capacitance(method='ana')
     C = pair.capacitance(method='fdm')
     assert C == approx(expected, rel=0.05, abs=0.1e-12)
@@ -177,10 +188,11 @@ def test_two_wire_capacitance_fdm_diagonal():
 def test_two_wire_capacitance_fdm_near():
     rw = 1e-3
     s = 3e-3
-    wires = [(-0.5 * s, 0, rw), (0.5 * s, 0, rw)]
-    pair = mtl.WireMtl(wires)
-    expected = pair.capacitance(method='ana')
-    C = pair.capacitance(method='fdm', fdm_params={'dx': rw / 12})
+    wires = [Wire(-0.5 * s, 0, rw)]
+    ref = Wire(0.5 * s, 0, rw)
+    pair = mtl.WireMtl(wires, ref)
+    expected = mtl.wire_capacitance(s, rw)
+    C = pair.capacitance(method='fdm', fdm_params={'dx': rw / 10})
     assert C == approx(expected, rel=0.05, abs=0.1e-12)
 
 
@@ -189,8 +201,9 @@ def test_two_wire_capacitance_fdm_rw2():
     rw1 = 1e-3
     rw2 = 2e-3
     s = 20e-3
-    wires = [(-0.5 * s, 0, rw1), (0.5 * s, 0, rw2)]
-    pair = mtl.WireMtl(wires)
+    wires = [Wire(-0.5 * s, 0, rw1)]
+    ref = Wire(0.5 * s, 0, rw2)
+    pair = mtl.WireMtl(wires, ref)
     expected = pair.capacitance(method='ana')
     print(expected)
     C = pair.capacitance(method='fdm')
@@ -201,9 +214,10 @@ def test_three_wire_inductance():
     # Paul MTL P5.4
     rw = 7.5
     s = 50
-    wires = [(0, 0, rw), (-s, 0, rw), (s, 0, rw)]
-    pair = mtl.WireMtl(wires)
-    L = pair.inductance()
+    wires = [Wire(s, 0, rw), Wire(-s, 0, rw)]
+    ref = Wire(0, 0, rw)
+    bus = mtl.WireMtl(wires, ref)
+    L = bus.inductance()
     expected = np.array([[0.759, 0.241], [0.241, 0.759]]) * 1e-6
     assert L == approx(expected, rel=0.001)
 
@@ -212,9 +226,10 @@ def test_three_wire_capacitance():
     # Paul MTL P5.4
     rw = 7.5
     s = 50
-    wires = [(0, 0, rw), (-s, 0, rw), (s, 0, rw)]
-    pair = mtl.WireMtl(wires)
-    C = pair.capacitance()
+    wires = [Wire(s, 0, rw), Wire(-s, 0, rw)]
+    ref = Wire(0, 0, rw)
+    bus = mtl.WireMtl(wires, ref)
+    C = bus.capacitance()
     expected = np.array([[16.3, -5.17], [-5.17, 16.3]]) * 1e-12
     assert C == approx(expected, rel=0.001, abs=1e-14)
 
@@ -224,8 +239,9 @@ def test_two_wire_inductance_plane():
     rw = 0.04064
     s = 2
     h = 2
-    wires = [(-0.5 * s, h, rw), (0.5 * s, h, rw)]
-    pair = mtl.WireMtl(wires, ref='plane')
+    wires = [Wire(-0.5 * s, h, rw), Wire(0.5 * s, h, rw)]
+    ref = Plane()
+    pair = mtl.WireMtl(wires, ref)
     L = pair.inductance()
     expected = np.array([[0.918, 0.161], [0.161, 0.918]]) * 1e-6
     assert L == approx(expected, rel=0.001)
@@ -236,19 +252,12 @@ def test_two_wire_capacitance_plane():
     rw = 0.04064
     s = 2
     h = 2
-    wires = [(-0.5 * s, h, rw), (0.5 * s, h, rw)]
-    pair = mtl.WireMtl(wires, ref='plane')
+    wires = [Wire(-0.5 * s, h, rw), Wire(0.5 * s, h, rw)]
+    ref = Plane()
+    pair = mtl.WireMtl(wires, ref)
     C = pair.capacitance()
     expected = np.array([[12.5, -2.19], [-2.19, 12.5]]) * 1e-12
     assert C == approx(expected, rel=0.001, abs=1e-14)
-
-
-def test_two_wire_shield_no_radius():
-    rw = 0.1905e-3
-    s = 4 * rw
-    wires = [(-0.5 * s, 0, rw), (0.5 * s, 0, rw)]
-    with pytest.raises(Exception):
-        mtl.WireMtl(wires, ref='shield')
 
 
 def test_two_wire_inductance_shield():
@@ -256,8 +265,9 @@ def test_two_wire_inductance_shield():
     rw = 0.1905e-3
     s = 4 * rw
     rs = 4 * rw
-    wires = [(-0.5 * s, 0, rw), (0.5 * s, 0, rw)]
-    pair = mtl.WireMtl(wires, ref='shield', rs=rs)
+    wires = [Wire(-0.5 * s, 0, rw), Wire(0.5 * s, 0, rw)]
+    ref = Shield(rs)
+    pair = mtl.WireMtl(wires, ref)
     L = pair.inductance()
     expected = np.array([[0.2197, 0.0446], [0.0446, 0.2197]]) * 1e-6
     assert L == approx(expected, rel=0.001)
@@ -268,8 +278,9 @@ def test_two_wire_capacitance_shield():
     rw = 0.1905e-3
     s = 4 * rw
     rs = 4 * rw
-    wires = [(-0.5 * s, 0, rw), (0.5 * s, 0, rw)]
-    pair = mtl.WireMtl(wires, ref='shield', rs=rs)
+    wires = [Wire(-0.5 * s, 0, rw), Wire(0.5 * s, 0, rw)]
+    ref = Shield(rs)
+    pair = mtl.WireMtl(wires, ref)
     C = pair.capacitance()
     expected = np.array([[52.8, -10.73], [-10.73, 52.8]]) * 1e-12
     assert C == approx(expected, rel=0.001, abs=1e-14)
@@ -279,8 +290,9 @@ def test_one_wire_capacitance_shield():
     rw = 0.5e-3
     rs = 4e-3
     er = 5.2
-    wires = [(0, 0, rw)]
-    cable = mtl.WireMtl(wires, er=er, ref='shield', rs=rs)
+    wires = [Wire(0, 0, rw)]
+    ref = Shield(rs)
+    cable = mtl.WireMtl(wires, ref, er)
     C = cable.capacitance()
     expected = coax.capacitance(rw, rs, er)
     assert C == approx(expected, rel=0.001, abs=1e-14)
